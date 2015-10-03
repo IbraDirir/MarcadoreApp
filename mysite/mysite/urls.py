e7819a20039777f2ed6ecdb1c60bf59bd1293c7e
IbraDirir <khaliildirir@gmail.com>
2015-10-03 17:54:29 +0300

    yeah it worked now adding url to the  project again

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,13 +19,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
-from django.conf.urls import include, url
-from django.contrib import admin
+url(r'^', include('marcador.urls')),
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('marcador.urls')),
-]
 
 
 
